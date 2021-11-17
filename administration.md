289ea1b3ff66c933ac0c1dbfa6c321fffd7cc003
strukteon <sensentod@gmail.com>
2021-11-17 10:47:17 +0100

    added image select functionality to product page, added some translations

--- administration.md
+++ administration.md
@@ -5,3 +5,10 @@ assign to ``products``
 
 custom fields:
 * ``vs_product_taste`` type ``text editor``
+* ``vs_product_good_with`` type ``select field``
+    * multi select
+    * options:
+        1. ``cheese``: ``Käse``
+        2. ``red_meat``: ``Rotes Fleisch``
+        2. ``light_meat``: ``Helles Fleisch``
+        2. ``vegetables``: ``Gemüse``
--- plugins/VinoshopTheme/src/Resources/app/storefront/src/main.js
+++ plugins/VinoshopTheme/src/Resources/app/storefront/src/main.js
@@ -1,4 +1,6 @@
 import ProductPagePlugin from "./scripts/pages/product-page-plugin.plugin";
+import NumberInputPlugin from "./scripts/number-input-plugin.plugin";
 
 const PluginManager = window.PluginManager;
 PluginManager.register('ProductPagePlugin', ProductPagePlugin, '[data-product-page-plugin]');
+PluginManager.register('NumberInputPlugin', NumberInputPlugin, '[data-number-input-plugin]');
