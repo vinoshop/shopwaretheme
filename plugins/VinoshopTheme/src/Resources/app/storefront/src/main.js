6daed06acf1f5d818a0b95e788ed9f892caa82bc
Nils <sensentod@gmail.com>
2021-12-01 15:37:43 +0100

    added search to header

--- plugins/VinoshopTheme/src/Resources/app/storefront/src/main.js
+++ plugins/VinoshopTheme/src/Resources/app/storefront/src/main.js
@@ -6,3 +6,10 @@ const PluginManager = window.PluginManager;
 PluginManager.register('ProductPagePlugin', ProductPagePlugin, '[data-product-page-plugin]');
 PluginManager.register('NumberInputPlugin', NumberInputPlugin, '[data-number-input-plugin]');
 PluginManager.register('NumberInputPlugin', HeaderPlugin, '[data-header-plugin]');
+
+function getElementByXPath(xpath_identifier, elem) {
+    return document.evaluate(
+        xpath_identifier, elem, null,
+        XPathResult.FIRST_ORDERED_NODE_TYPE, null
+    ).singleNodeValue
+}
--- plugins/VinoshopTheme/src/Resources/app/storefront/src/scripts/header-plugin.plugin.js
+++ plugins/VinoshopTheme/src/Resources/app/storefront/src/scripts/header-plugin.plugin.js
@@ -2,8 +2,21 @@ import Plugin from 'src/plugin-system/plugin.class';
 
 export default class HeaderPlugin extends Plugin {
     init() {
+        let header = document.getElementsByClassName("header-main")[0];
+
         this.el.addEventListener("click", () => document.body.classList.add("mobile-nav-open"));
         document.getElementsByClassName("btn-close-mobile-nav-overview")[0]
             .addEventListener("click", () => document.body.classList.remove("mobile-nav-open"));
+
+        let searchOverlay = document.getElementsByClassName("search-overlay")[0];
+        [...header.getElementsByClassName("search-menu")].forEach(x => x.addEventListener("click", () => {
+            searchOverlay.classList.add("search-visible");
+        }));
+        searchOverlay.addEventListener("click", event => {
+            searchOverlay.classList.remove("search-visible")
+        })
+        document.getElementsByClassName("header-search-form")[0].addEventListener("click", event => {
+            event.stopPropagation();
+        })
     }
 }
