d9f01834763e5015bfeab6a3b0203a30efd4730e
JakobMatijasevic <71332745+JakobMatijasevic@users.noreply.github.com>
2021-12-09 23:35:01 +0100

    winzerpage styling und navbar

--- plugins/VinoshopTheme/src/Storefront/Controller/WinzerpageController.php
+++ plugins/VinoshopTheme/src/Storefront/Controller/WinzerpageController.php
@@ -8,19 +8,37 @@ use Shopware\Storefront\Controller\StorefrontController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Shopware\Storefront\Page\Navigation\NavigationPageLoaderInterface;
+use Shopware\Storefront\Pagelet\Menu\Offcanvas\MenuOffcanvasPageletLoaderInterface;
+use Shopware\Core\Framework\Routing\Annotation\Since;
+use Shopware\Storefront\Framework\Cache\Annotation\HttpCache;
 
 /**
  * @RouteScope(scopes={"storefront"})
  */
 class WinzerpageController extends StorefrontController
 {
-    /**
-     * @Route("/winzer", name="frontend.winzer", methods={"GET"})
-     */
-    public function renderWinzerpage(): Response
+    private NavigationPageLoaderInterface $navigationPageLoader;
+    private MenuOffcanvasPageletLoaderInterface $offcanvasLoader;
+
+    public function __construct(
+        NavigationPageLoaderInterface       $navigationPageLoader,
+        MenuOffcanvasPageletLoaderInterface $offcanvasLoader
+    )
     {
-        return $this->renderStorefront('@VinoshopTheme/storefront/page/winzer/winzerpage.html.twig', [
-            'example' => 'Hello world'
-        ]);
+        $this->navigationPageLoader = $navigationPageLoader;
+        $this->offcanvasLoader = $offcanvasLoader;
+    }
+
+    /**
+     * @Route("/winzer", name="frontend.winzer", options={"seo"="true"}, methods={"GET"})
+     */
+    public function renderWinzerpage(Request $request, SalesChannelContext $context): ?Response
+    {
+        $page = $this->navigationPageLoader->load($request, $context);
+
+        return $this->renderStorefront('@VinoshopTheme/storefront/page/winzer/winzerpage.html.twig',
+            ['page' => $page],
+        );
     }
 }
