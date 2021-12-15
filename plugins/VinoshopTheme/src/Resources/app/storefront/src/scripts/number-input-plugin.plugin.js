7f715d02da8e2e3edfb5549cfd7055f09f8e4d6f
Nils <sensentod@gmail.com>
2021-12-15 16:47:07 +0100

    changed buy widget inputs to bootstrap default

--- plugins/VinoshopTheme/src/Resources/app/storefront/src/scripts/number-input-plugin.plugin.js
+++ plugins/VinoshopTheme/src/Resources/app/storefront/src/scripts/number-input-plugin.plugin.js
@@ -3,12 +3,12 @@ import Plugin from 'src/plugin-system/plugin.class';
 export default class NumberInputPlugin extends Plugin {
     init() {
         const el = this.el;
-        const input = el.getElementsByClassName("elem-input")[0];
+        const input = el.getElementsByClassName("elem-number-input")[0];
         input.addEventListener("input", () => {
             console.log(input.value.replaceAll(/[^\d]/g, ''), input.value)
             if (input.value.replaceAll(/[^\d]/g, '') !== input.value)
                 input.value = input.value.replaceAll(/[^\d]/g, '');
-        })
+        });
         el.getElementsByClassName("elem-input-number-increase")[0]
             .addEventListener("click", () => input.value = Math.min(Number(input.value) + 1, input.getAttribute("max")));
         el.getElementsByClassName("elem-input-number-decrease")[0]
