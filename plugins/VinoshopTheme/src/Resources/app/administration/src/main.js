b7db85781efbcda607deb2c8966b46f051d98b54
JakobMatijasevic <71332745+JakobMatijasevic@users.noreply.github.com>
2022-03-22 21:25:42 +0100

    landingpage bugfix winzerpage bugfix

--- plugins/VinoshopTheme/src/Resources/app/administration/src/main.js
+++ plugins/VinoshopTheme/src/Resources/app/administration/src/main.js
@@ -1,3 +1,2 @@
 //<plugin root>/src/Resources/app/administration/src/main.js
 
-import './module/sw-cms/blocks/commerce/product-with-winzer-three-column'
--- plugins/VinoshopTheme/src/Resources/app/administration/src/module/sw-cms/elements/dailymotion/preview/index.js
+++
@@ -1,6 +0,0 @@
-import template from './sw-cms-el-preview-dailymotion.html.twig';
-import './sw-cms-el-preview-dailymotion.scss';
-
-Shopware.Component.register('sw-cms-el-preview-dailymotion', {
-    template
-});
